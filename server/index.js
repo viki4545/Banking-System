68e4aa780a4da00f6f83c021ea6ef1dd3839e7d9
Vicky <vickygupta9987@gmail.com>
2023-12-06 16:04:05 +0530

    server changes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "https://master--delightful-toffee-5a8d1a.netlify.app/",
+    origin: "https://master--delightful-toffee-5a8d1a.netlify.app",
     credentials: true,
   })
 );
